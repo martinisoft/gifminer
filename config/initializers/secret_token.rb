3c45408f0fdc2e082f7d4ed36fa855382693cd47
Aaron Kalin <akalin@martinisoftware.com>
2013-02-12 08:53:05 +0100

    Initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gifminer::Application.config.secret_token = '9f3aad1da335aba9ca2bea700a111366a12bef80d8958a1dd738e88b4506a54c2ed5fabd0a305eb48f1838a33eeceee926e15314924a65c6f07c8fd2f914fd27'
