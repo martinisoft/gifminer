1b578a0727b81b9a9cfec1abb0502b3809aa5fde
Marc Heiligers <marc@eternal.co.za>
2013-03-01 20:11:24 +0200

    Implement some devise recommendations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,16 @@ Gifminer::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Devise
+  # 1. Ensure you have defined default url options in your environments files. Here
+  #    is an example of default_url_options appropriate for a development environment
+  #    in config/environments/development.rb:
+
+  #      config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  #    In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -34,4 +44,5 @@ Gifminer::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
 end
